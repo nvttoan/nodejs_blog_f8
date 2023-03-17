1fce155370f341b24fc6c97d928c138976576370
toanphysics <nvttoan2017@gmail.com>
2023-03-18 01:21:34 +0700

    them news va home

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan =require('morgan');
+const path = require('path')
+const { engine } = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+
+//HTTP logger
+app.use(morgan('combined'))
+
+//template engine
+//{extname: '.hbs'} change .handblebars==.hbs
+app.engine('hbs', engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+//console.log('PATH:',path.join(__dirname, 'resources/views'))
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
