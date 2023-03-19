72d31cd1431c8955d89c2a43e491c4427d259cdc
toanphysics <nvttoan2017@gmail.com>
2023-03-20 02:07:20 +0700

    controller and routes

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ const { engine } = require('express-handlebars');
 
 const app = express();
 const port = 3000;
+const route = require('./routes');
 
 // render logo.png of http://localhost:3000/img/logo.png
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 //HTTP logger
 app.use(morgan('combined'))
@@ -19,14 +25,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 //console.log('PATH:',path.join(__dirname, 'resources/views'))
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+//route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
